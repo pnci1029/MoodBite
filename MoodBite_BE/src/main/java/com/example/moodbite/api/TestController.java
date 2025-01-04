40b757ed8ae4f7f1fbc98ab5f7205621a7568725
chhong <pnci1029@gmail.com>
2025-01-04 14:43:54 +0900

    [feature] 시크릿값 테스트

--- MoodBite_BE/src/main/java/com/example/moodbite/api/TestController.java
+++ MoodBite_BE/src/main/java/com/example/moodbite/api/TestController.java
@@ -1,12 +1,22 @@
 package com.example.moodbite.api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 public class TestController {
+
+    @Value("${openai.api.key}")
+    private String apiKey;
+
     @GetMapping("/")
     public String test() {
+
+        System.out.println("apiKey = " + apiKey);
+
         return "12312";
     }
 }
