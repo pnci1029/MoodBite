f411d69d2cd173e8eb7e269d2f83b06770a14919
chhong <pnci1029@gmail.com>
2025-01-04 15:05:32 +0900

    [feature] 통신테스트중

--- MoodBite_BE/src/main/java/com/example/moodbite/api/TestController.java
+++ MoodBite_BE/src/main/java/com/example/moodbite/api/TestController.java
@@ -1,22 +1,51 @@
 package com.example.moodbite.api;
 
+import com.example.moodbite.api.executed.dto.ChatRequest;
+import com.example.moodbite.api.executed.dto.ChatResponse;
+import com.example.moodbite.config.OpenAiConfig;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.Map;
 
-@RestController
+
+@RestController @RequiredArgsConstructor
 public class TestController {
 
+    private final OpenAiConfig openAiConfig;
     @Value("${openai.api.key}")
     private String apiKey;
 
+    @Value("${openai.model}")
+    private String model;
+
+    @Value("${openai.api.url}")
+    private String url;
+
+
     @GetMapping("/")
-    public String test() {
+    public String test(@RequestParam String prompt) {
+        System.out.println("prompt = " + prompt);
+        HttpHeaders headers = openAiConfig.httpHeaders();
 
-        System.out.println("apiKey = " + apiKey);
+        // Create request
+        ChatRequest chatRequest = new ChatRequest(model, prompt);
 
-        return "12312";
+        // 통신을 위한 RestTemplate 구성하기
+        HttpEntity<ChatRequest> requestEntity = new HttpEntity<>(chatRequest, headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+        ChatResponse response = restTemplate.postForObject(url, requestEntity, ChatResponse.class);
+
+        if (response == null || response.getChoices() == null || response.getChoices().isEmpty()) {
+            throw new RuntimeException();
+        }
+
+        return response.getChoices().get(0).getMessage().getContent();
     }
 }
