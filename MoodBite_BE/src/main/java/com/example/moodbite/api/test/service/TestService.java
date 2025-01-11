6c4b61a99f7bc97c3a9ab67070aff461099a8904
chhong <pnci1029@gmail.com>
2025-01-11 13:04:56 +0900

    [feature] 코틀린 전환 자바 파일 제거

--- MoodBite_BE/src/main/java/com/example/moodbite/api/test/service/TestService.java
+++
@@ -1,8 +0,0 @@
-package com.example.moodbite.api.test.service;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service @RequiredArgsConstructor
-public class TestService {
-}
