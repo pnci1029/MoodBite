2a116e4d78666c2bae2a3bd4733f8b206e1960b7
chhong <pnci1029@gmail.com>
2025-01-11 13:10:05 +0900

    [feature] 프롬프트 요청 서비스 분리

--- MoodBite_BE/src/main/java/com/example/moodbite/api/test/controller/TestController.kt
+++ MoodBite_BE/src/main/java/com/example/moodbite/api/test/controller/TestController.kt
@@ -3,7 +3,6 @@ package com.example.moodbite.api.test.controller
 import com.example.moodbite.api.executed.dto.ChatRequest
 import com.example.moodbite.api.executed.dto.ChatResponse
 import com.example.moodbite.api.test.service.TestService
-import com.example.moodbite.config.OpenAiConfig
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -13,14 +12,9 @@ import org.springframework.web.client.RestTemplate
 
 @RestController
 class TestController(
-    private val openAiConfig: OpenAiConfig,
-    private val testService: TestService
+    private val testService: TestService,
 ) {
-    @Value("\${openai.model}")
-    private lateinit var model: String
 
-    @Value("\${openai.api.url}")
-    private lateinit var url: String
 
 //    @PostMapping("/result")
 //    fun getTestResult(
@@ -33,18 +27,19 @@ class TestController(
     @GetMapping("/")
     fun test(@RequestParam prompt: String): String {
         println("prompt = $prompt")
-        val headers = openAiConfig.httpHeaders()
+//        val headers = openAiConfig.httpHeaders()
+//        // Create request
+//        val chatRequest = ChatRequest(model, prompt)
+//
+//        // 통신을 위한 RestTemplate 구성하기
+//        val requestEntity = HttpEntity(chatRequest, headers)
+//
+//        val restTemplate = RestTemplate()
+//        val response = restTemplate.postForObject(url, requestEntity, ChatResponse::class.java)
 
-        // Create request
-        val chatRequest = ChatRequest(model, prompt)
+//        return response?.choices?.firstOrNull()?.message?.content
+//            ?: throw RuntimeException("Failed to get response from OpenAI")
+        return testService.getResult(prompt)
 
-        // 통신을 위한 RestTemplate 구성하기
-        val requestEntity = HttpEntity(chatRequest, headers)
-
-        val restTemplate = RestTemplate()
-        val response = restTemplate.postForObject(url, requestEntity, ChatResponse::class.java)
-
-        return response?.choices?.firstOrNull()?.message?.content
-            ?: throw RuntimeException("Failed to get response from OpenAI")
     }
 }
