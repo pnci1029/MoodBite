d1b624c6dae6a570b8fd1a89511e861b8ea0c93f
chhong <pnci1029@gmail.com>
2025-01-16 19:16:35 +0900

    [feature] 요청 DTO

---
+++ MoodBite_BE/src/main/java/com/example/moodbite/api/test/dto/request/TestRequestDTO.kt
@@ -0,0 +1,9 @@
+package com.example.moodbite.api.test.dto.request
+
+data class TestRequestDTO(
+    val tiredScore: Int,
+    val angerScore: Int,
+    val stressScore: Int,
+    val appetiteScore: Int,
+
+)
