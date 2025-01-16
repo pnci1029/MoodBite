5485b4e52fd8eedfd7d286751caefca31963ef8b
chhong <pnci1029@gmail.com>
2025-01-16 19:16:37 +0900

    [feature] 요청 DTO

--- MoodBite_BE/src/main/java/com/example/moodbite/api/test/controller/TestController.kt
+++ MoodBite_BE/src/main/java/com/example/moodbite/api/test/controller/TestController.kt
@@ -1,8 +1,9 @@
 package com.example.moodbite.api.test.controller
 
+import com.example.moodbite.api.test.dto.request.TestRequestDTO
 import com.example.moodbite.api.test.service.TestService
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -11,8 +12,8 @@ class TestController(
 ) {
 
     @GetMapping("/")
-    fun test(@RequestParam prompt: String): String {
-        return testService.getResult(prompt)
+    fun test(@RequestBody dto: TestRequestDTO): String {
+        return testService.getResult(dto)
 
     }
 }
