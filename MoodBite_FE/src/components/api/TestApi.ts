2bbad041075a181b9a329d2eadce6144b84be6be
chhong <pnci1029@gmail.com>
2025-01-27 16:58:27 +0900

    [feature] 요청 API 수정

--- MoodBite_FE/src/components/api/TestApi.ts
+++ MoodBite_FE/src/components/api/TestApi.ts
@@ -2,8 +2,8 @@ import {TestResultPostDTO} from "../../types/test";
 import {MainApi} from "./MainApi";
 
 export class TestApi {
-    static url = `${process.env.REACT_APP_BASE_URL}/community`;
+    static url = `${process.env.REACT_APP_BASE_URL}/test`;
 
     static submitTestResult = (dto:TestResultPostDTO) => () =>
-        MainApi.api.post(`${TestApi.url}/post`, dto);
+        MainApi.api.post(`${TestApi.url}`, dto);
 }
