aa5b6f63fa42d8a0f257e6facdd4a763473c9fe9
chhong <pnci1029@gmail.com>
2025-01-27 16:32:36 +0900

    [feature] 서버 요청용 컴포넌트 추가

--- MoodBite_FE/src/slice/index.ts
+++ MoodBite_FE/src/slice/index.ts
@@ -1,9 +1,19 @@
 import {Action, configureStore, ThunkAction} from "@reduxjs/toolkit";
+import {AxiosResponse} from "axios";
 
 export const store = configureStore({
     reducer: {
     },
 });
+
+export const executePromise = async <T>(f: () => Promise<AxiosResponse<T>>) => {
+    try {
+        return await f();
+    } catch (error: any) {
+        throw new Error(JSON.stringify(error?.response?.data));
+    }
+};
+
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
