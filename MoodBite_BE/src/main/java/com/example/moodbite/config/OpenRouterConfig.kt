437c5218d564f666a998ee75b3ae0a95e1d3fde0
chhong <pnci1029@gmail.com>
2025-03-17 11:16:50 +0900

    [feature] 이것저것 수정4

--- MoodBite_BE/src/main/java/com/example/moodbite/config/OpenRouterConfig.kt
+++ MoodBite_BE/src/main/java/com/example/moodbite/config/OpenRouterConfig.kt
@@ -1,7 +1,6 @@
 package com.example.moodbite.config
 
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
@@ -11,7 +10,6 @@ class OpenRouterConfig {
     @Value("\${openrouter.api.key}")
     private lateinit var secretKey: String
 
-    @Bean
     fun httpHeaders(): HttpHeaders {
         return HttpHeaders().apply {
             set("Authorization", "Bearer $secretKey")
