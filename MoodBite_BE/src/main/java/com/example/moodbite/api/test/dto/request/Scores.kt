1cda8d8f6c48bf1fdaa0315b22de9508ee424c74
chhong <pnci1029@gmail.com>
2025-03-17 11:16:21 +0900

    [feature] 이것저것 수정1

---
+++ MoodBite_BE/src/main/java/com/example/moodbite/api/test/dto/request/Scores.kt
@@ -0,0 +1,9 @@
+package com.example.moodbite.api.test.dto.request
+
+data class Scores (
+    val tiredScore: Int,
+    val angerScore: Int,
+    val stressScore: Int,
+    val appetiteScore: Int,
+    val budget: Int,
+)
