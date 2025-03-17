95438ee24377fd5bde28089bbb1ca96ef99c3292
chhong <pnci1029@gmail.com>
2025-03-17 11:16:59 +0900

    [feature] 이것저것 수정5

--- MoodBite_BE/src/main/java/com/example/moodbite/api/test/controller/TestController.kt
+++ MoodBite_BE/src/main/java/com/example/moodbite/api/test/controller/TestController.kt
@@ -4,14 +4,16 @@ import com.example.moodbite.api.test.dto.request.TestRequestDTO
 import com.example.moodbite.api.test.service.TestService
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/api")
 class TestController(
     private val testService: TestService,
 ) {
 
-    @PostMapping("/")
+    @PostMapping("/test")
     fun test(@RequestBody dto: TestRequestDTO): String {
         return testService.getResult(dto)
 
--- MoodBite_BE/src/main/java/com/example/moodbite/api/test/dto/request/TestRequestDTO.kt
+++ MoodBite_BE/src/main/java/com/example/moodbite/api/test/dto/request/TestRequestDTO.kt
@@ -1,14 +1,15 @@
 package com.example.moodbite.api.test.dto.request
 
 import com.example.moodbite.api.test.domain.enums.Dining
+import com.example.moodbite.api.test.domain.enums.MealTime
 
 data class TestRequestDTO(
     val tiredScore: Int,
     val angerScore: Int,
     val stressScore: Int,
     val appetiteScore: Int,
-    val dining: Dining,
     val budget: Int,
-    val mealTime: String,
+    val dining: Dining,
+    val mealTime: MealTime,
 
 )
